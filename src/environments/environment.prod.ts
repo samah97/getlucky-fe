b5973e4894d0a48817aab178a0510737a320ea3a
Samah <samahdeveloper97@gmail.com>
2024-01-21 21:34:05 +0200

    Add Environment Variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.stage.ts
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:8080',
+    apiUrl: 'https://srv466142.hstgr.cloud:8080',
     apiVersion:'v1/',
     siteKey:'6LcwBlIpAAAAANy5WNfRwPf4NF1brvCkbYXHiaat'
 };
