7c4e50cf02db0676449fcab228b039c944847a9c
rw00 <raafatwahb@gmail.com>
2024-01-21 17:33:00 +0100

    fix class transformer issue

--- src/app/models/order-item.ts
+++ src/app/models/order-item.ts
@@ -1,13 +1,10 @@
-import {Expose, Type} from "class-transformer";
-
-export interface ItemSummary{
-    id:string,
-    name:string
-    price:number
+export interface ItemSummary {
+    id: string;
+    name: string;
+    price: number;
 }
 
 export class OrderItem {
-
-  quantity:number;
-  item:ItemSummary;
+    quantity: number;
+    item: ItemSummary;
 }
