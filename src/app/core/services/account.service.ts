83ba390717910f597c60a45377d7acc6aa15fb61
rw00 <raafatwahb@gmail.com>
2024-01-26 23:10:35 +0100

    fix reset pass

--- src/app/core/services/account.service.ts
+++ src/app/core/services/account.service.ts
@@ -1,31 +1,30 @@
 import { Injectable } from '@angular/core';
-import {Observable} from "rxjs";
-import {HttpClient} from "@angular/common/http";
-import {environment} from "../../../environments/environment";
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
-const apiRoute = environment.apiVersion+'accounts'
+const apiRoute = environment.apiVersion + 'accounts';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  constructor(private readonly httpClient: HttpClient) {}
 
-   constructor(private readonly httpClient:HttpClient) { }
+  forgetPassword(email: string): Observable<any> {
+    return this.httpClient.post<any>(apiRoute + '/reset-pass', { email });
+  }
 
-    forgetPassword(email: string): Observable<any> {
-        return this.httpClient.post<any>(apiRoute+"reset-pass", { email });
-    }
+  resetPassword(password: string, userId: string, confirmationToken: string): Observable<any> {
+    return this.httpClient.post<any>(apiRoute + '/change-reset-pass/' + userId, {
+      resetToken: confirmationToken,
+      password
+    });
+  }
 
-    resetPassword(password: string, userId: string, confirmationToken: string): Observable<any> {
-        return this.httpClient.post<any>(apiRoute+"/change-reset-pass/" + userId, {
-            resetToken: confirmationToken,
-            password
-        });
-    }
-
-    confirmAccount(userId: string, confirmationToken: string): Observable<any> {
-        return this.httpClient.post<any>(apiRoute+"/confirm-account/" + userId, {
-            token: confirmationToken,
-        });
-    }
+  confirmAccount(userId: string, confirmationToken: string): Observable<any> {
+    return this.httpClient.post<any>(apiRoute + '/confirm-account/' + userId, {
+      token: confirmationToken
+    });
+  }
 }
