ba889c46e9deda43a3a01854a7573bdea355d7c3
rw00 <raafatwahb@gmail.com>
2024-01-30 19:26:33 +0100

    fix typos in profile and product img in cart

--- src/app/order/my-orders/my-orders.component.ts
+++ src/app/order/my-orders/my-orders.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { OrderService } from "../../core/services/order.service";
-import { Order } from "../../models/order";
-import { ProductsService } from "../../core/services/products.service";
+import { OrderService } from '../../core/services/order.service';
+import { Order } from '../../models/order';
+import { ProductsService } from '../../core/services/products.service';
 
 @Component({
   selector: 'app-my-orders',
@@ -13,8 +13,10 @@ export class MyOrdersComponent implements OnInit {
   pendingPaymentOrders: Order[];
   products: any[] = [];
 
-  constructor(private orderService: OrderService, private productsService: ProductsService) {
-  }
+  constructor(
+    private orderService: OrderService,
+    private productsService: ProductsService
+  ) {}
 
   ngOnInit(): void {
     this.initData();
@@ -23,11 +25,14 @@ export class MyOrdersComponent implements OnInit {
   initData() {
     this.orderService.myOrders().subscribe({
       next: (response) => {
-        this.paidOrders = response.filter(order => order.status === 'PAID');
-        this.pendingPaymentOrders = response.filter(order => order.status === 'UNPAID' || order.status === 'PAYMENT_PENDING');
-        console.log(this.paidOrders);
+        this.paidOrders = response.filter((order) => order.status === 'PAID');
+        // TODO UNPAID = cart
+        // TODO PAYMENT_CONFIRMATION_PENDING = payment started but we didn't receive confirmation from Stripe yet
+        this.pendingPaymentOrders = response.filter(
+          (order) => order.status === 'UNPAID' || order.status === 'PAYMENT_CONFIRMATION_PENDING'
+        );
       }
-    })
+    });
     this.productsService.all().subscribe((result) => {
       this.products = result;
     });
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Order } from "../models/order";
-import { OrderStatus } from "../core/enums/order-status";
-import { OrderService } from "../core/services/order.service";
+import { Order } from '../models/order';
+import { OrderStatus } from '../core/enums/order-status';
+import { OrderService } from '../core/services/order.service';
 
 @Component({
   selector: 'app-order',
@@ -9,23 +9,21 @@ import { OrderService } from "../core/services/order.service";
   styleUrl: './order.component.scss'
 })
 export class OrderComponent implements OnInit {
-
   @Input() order: Order;
-  totalPrice: number
+  totalPrice: number;
   showPaymentButton: boolean = false;
 
-  constructor(private orderService: OrderService) {
-  }
+  constructor(private orderService: OrderService) {}
 
   ngOnInit(): void {
-    const orderStatus = OrderStatus[this.order.status as keyof typeof OrderStatus]
+    const orderStatus = OrderStatus[this.order.status as keyof typeof OrderStatus];
     if (orderStatus == OrderStatus.UNPAID || orderStatus == OrderStatus.PAYMENT_PENDING) {
       this.showPaymentButton = true;
     }
   }
-  getTotalPrice():number {
+  getTotalPrice(): number {
     this.totalPrice = this.order.items.reduce((total, orderItem) => {
-      return total + (orderItem.quantity * orderItem.item.biddingPrice);
+      return total + orderItem.quantity * orderItem.item.biddingPrice;
     }, 0);
     return 0;
   }
